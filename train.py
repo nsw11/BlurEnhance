fb692044d26c83281bc1ccc9f4d2061be5580075
nsw11 <39717120+nsw11@users.noreply.github.com>
2022-09-24 09:30:39 -0700

    Update train.py

--- train.py
+++ train.py
@@ -31,8 +31,6 @@ def train(opt, data_loader, model, visualizer):
 				errors = model.get_current_errors()
 				t = (time.time() - iter_start_time) / opt.batchSize
 				visualizer.print_current_errors(epoch, epoch_iter, errors, t)
-				if opt.display_id > 0:
-					visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
 
 			if total_steps % opt.save_latest_freq == 0:
 				print('saving the latest model (epoch %d, total_steps %d)' % (epoch, total_steps))
